f851a3550d6fe3756063b56b3eb2273f3b4b7f31
hersson ceballos <herssonceballos@gmail.com>
2020-08-05 22:13:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,9 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
   
     
