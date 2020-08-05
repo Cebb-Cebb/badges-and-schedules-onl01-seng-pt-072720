2f82f644a26f03cafdee3c08a674efa272e02723
hersson ceballos <herssonceballos@gmail.com>
2020-08-05 21:36:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,6 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(speaker)
-  puts "Hello, my name is #{speaker}"
+  "Hello, my name is #{speaker}"
 end
-
 
