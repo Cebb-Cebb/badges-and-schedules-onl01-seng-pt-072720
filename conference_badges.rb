529eebaa6d85c3aa36295c940a3f7d41d148b17d
hersson ceballos <herssonceballos@gmail.com>
2020-08-05 21:34:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,10 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(speakers)
   speakers.each do
-    puts "Hello, my name is #{name}"
+    puts "Hello, my name is #{sepakers}"
   end
 end
+
+
